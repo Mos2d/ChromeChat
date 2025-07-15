2743dd6f57cf0efc33b5db6abee883839b2c9d55
Youssof Mohamed <90064094+Mos2d@users.noreply.github.com>
2025-07-15 03:03:55 -0400

    added particle system
    added particles in the background for nice looks

--- newtab.js
+++ newtab.js
@@ -40,3 +40,29 @@ fetch(`https://api.alquran.cloud/v1/ayah/${randomAyah}/editions/ar.quran-simple,
     console.error('Verse fetch error:', err);
     verseEl.innerText = 'Could not load verse.';
   });
+
+  particlesJS('particles-layer1', {
+  particles: {
+    number: { value: 10 },
+    size: { value: 1.5 },
+    color: { value: '#ffffff' },
+    opacity: { value: 0.5 },
+    move: { speed: 0.8, direction: 'none' },
+    line_linked: { enable: false }
+  },
+  interactivity: { events: { onhover: { enable: false }, onclick: { enable: false } } }
+});
+
+particlesJS('particles-layer2', {
+  particles: {
+    number: { value: 10 },
+    size: { value: 1 },
+    color: { value: '#ffffff' },
+    opacity: { value: 0.2 },
+    move: { speed: 0.4, direction: 'none' },
+    line_linked: { enable: false }
+  },
+  interactivity: { events: { onhover: { enable: false }, onclick: { enable: false } } }
+});
+
+
